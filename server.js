0af63439a5dfb847f1156cb77bec6d9807f028ce
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-01-22 19:29:13 +0900

    remove: credentials 삭제

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(
       "https://main.d2n5k148zzb8mj.amplifyapp.com",
       "http://localhost:3000",
     ],
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
